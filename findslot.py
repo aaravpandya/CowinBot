68e1a0180bb7bfb6f80ae3e6f16d4eeb28f1b7ab
Aarav Pandya <pandya.aarav.97@gmail.com>
2021-05-09 21:08:52 +0530

    init

---
+++ findslot.py
@@ -0,0 +1,87 @@
+import requests
+import json
+import webbrowser
+
+""" PARAMETERS """
+url_session = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=169&date=09-05-2021"
+url_getReCaptcha = "https://cdn-api.co-vin.in/api/v2/auth/getRecaptcha"
+url_schedule = "https://cdn-api.co-vin.in/api/v2/appointment/schedule"
+edgepath = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+beneficiaries = ["27388712323730"]
+slot_index = 0
+
+
+webbrowser.register('edge', None, webbrowser.BackgroundBrowser(edgepath))
+edge = webbrowser.get("edge")
+payload={}
+Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiI5NTFlNWVlOS03NDAzLTQwMWMtOGUyYy01Zjg5ZWJhYzdkYTQiLCJ1c2VyX2lkIjoiOTUxZTVlZTktNzQwMy00MDFjLThlMmMtNWY4OWViYWM3ZGE0IiwidXNlcl90eXBlIjoiQkVORUZJQ0lBUlkiLCJtb2JpbGVfbnVtYmVyIjo5ODk4Mjk4NjAzLCJiZW5lZmljaWFyeV9yZWZlcmVuY2VfaWQiOjI3Mzg4NzEyMzIzNzMwLCJzZWNyZXRfa2V5IjoiYjVjYWIxNjctNzk3Ny00ZGYxLTgwMjctYTYzYWExNDRmMDRlIiwidWEiOiJNb3ppbGxhLzUuMCAoV2luZG93cyBOVCAxMC4wOyBXaW42NDsgeDY0KSBBcHBsZVdlYktpdC81MzcuMzYgKEtIVE1MLCBsaWtlIEdlY2tvKSBDaHJvbWUvOTAuMC40NDMwLjkzIFNhZmFyaS81MzcuMzYgRWRnLzkwLjAuODE4LjU2IiwiZGF0ZV9tb2RpZmllZCI6IjIwMjEtMDUtMDlUMTU6MTA6MjEuMDM2WiIsImlhdCI6MTYyMDU3MzAyMSwiZXhwIjoxNjIwNTczOTIxfQ._6WbTO3vOmukAXalo-1H_CJl0fa-eMm7sRvysQaJOX0"
+headers = {
+  'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Microsoft Edge";v="90"',
+  'Accept': 'application/json, text/plain, */*',
+  'Authorization': 'Bearer '+Token,
+  'sec-ch-ua-mobile': '?0',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36 Edg/90.0.818.56'
+}
+
+def checkSessions():
+    response = requests.request("GET", url_session, headers=headers, data=payload)
+    print(response.status_code)
+    response = json.loads(response.text)
+    for center in response["centers"]:
+        for session in center["sessions"]:
+            if(session["available_capacity"]>0):
+                return True, center["center_id"], session["session_id"], session["slots"]
+
+def getRecaptcha():
+    payload = json.dumps({})
+    headers = {
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Microsoft Edge";v="90"',
+    'Accept': 'application/json, text/plain, */*',
+    'Authorization': 'Bearer '+Token,
+    'sec-ch-ua-mobile': '?0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36 Edg/90.0.818.56',
+    'Content-Type': 'application/json'
+    }
+    response = requests.request("POST", url_getReCaptcha, headers=headers, data=payload)
+    print(response.text)
+    svg = json.loads(response.text)["captcha"].replace("\\","")
+    return svg
+
+def schedule(center_id,captcha,session_id,slots):
+    payload = json.dumps({
+    "center_id": center_id,
+    "session_id": session_id,
+    "beneficiaries": beneficiaries,
+    "slot": slots[slot_index],
+    "captcha": captcha,
+    "dose": 1
+    })
+    headers = {
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Microsoft Edge";v="90"',
+    'Accept': 'application/json, text/plain, */*',
+    'Authorization': 'Bearer '+Token,
+    'sec-ch-ua-mobile': '?0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36 Edg/90.0.818.56',
+    'Content-Type': 'application/json'
+    }
+    response = requests.request("POST", url_schedule, headers=headers, data=payload)
+    print(response.status_code)
+    print(response.text)
+    return response.status_code
+
+while(True):
+    Available,center_id, session_id, slots = checkSessions()
+    if(Available):
+        svg = getRecaptcha()
+        with open("template.html",'r') as fp:
+            html = fp.read()
+        html = html.replace("Token", svg)
+        with open("index.html",'w') as fp:
+            fp.write(html)
+        edge.open_new_tab("C:\\Users\\pandy\\Dev\\Cowin\\index.html")
+        captcha = input()
+        schedule(center_id,captcha,session_id,slots)
+        break
+
+
+
